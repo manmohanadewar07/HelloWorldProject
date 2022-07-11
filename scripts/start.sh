64e6396b2c448d812edba627c029eca8f756f54d
Manmohan Adewar <66794727+manmohanadewar07@users.noreply.github.com>
2022-07-11 16:04:45 +0530

    Added comment to the file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-set -x
-npm run build
-set +x
-set -x
-npm start &
-sleep 10
-echo $! > .pidfile
-set +x
+#set -x
+npm run build #Run the npm build command
+#set +x
+#set -x
+npm start & #Start the application and runn server on local host on port 8081;
+sleep 10 #pause for 10s As I was getting issue to continue with other stages in Jenkins added this line.
+echo $! > .pidfile #store the pid for npm start process so that we can abort the code in future.
+#set +x
