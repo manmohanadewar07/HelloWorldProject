d369ce0a27a3b099b2c22a7dfe1d8a34fc44a114
Manmohan Adewar <66794727+manmohanadewar07@users.noreply.github.com>
2022-07-11 16:02:13 +0530

    Added comment to the file

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set -x
-head Response.txt
-rm Response.txt
-kill $(cat .pidfile)
+#set -x
+head Response.txt #Display the content of local file 
+rm Response.txt #Delete the local file
+kill $(cat .pidfile) #Stop the application
